721039e099c993c7b4745e0d0ff5ea8c0328709c
Matheus Sounier <matheus.sounier2024@gmail.com>
2026-03-18 11:16:20 -0400

    Add categorias e prevenção de bugs de produtos

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,13 +74,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_CONTAINER_NOME'),
+        'PORT': config('POSTGRES_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- fluxstore/models.py
+++ fluxstore/models.py
@@ -1,3 +1,24 @@
+# Arquivo que define como os dados da loja são organizados e salvos no banco de dados
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# Regra que garante que produtos invisíveis/desativados nunca apareçam para os clientes
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+# Representa uma categoria da loja, como "Eletrônicos" ou "Roupas"
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)      # Nome da categoria
+    slug = models.SlugField(max_length=255, unique=True)        # Texto que aparece na URL, ex: /eletronicos
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
